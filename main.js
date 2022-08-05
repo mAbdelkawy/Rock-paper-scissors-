c4110d3be054b3b3dac5e77a9b06636a034127d8
yokova <yokova01@gmail.com>
2022-08-05 21:44:20 +0200

    Code the javascript to make the header photo Match computer selection

--- main.js
+++ main.js
@@ -22,23 +22,27 @@
     function singleRound(){
 
         // assign the value of the functions to variables
-        let playerSelection = this.textContent ;
+        let playerSelection = this.classList.value ;
+        console.log(playerSelection);
         let computerSelection = getComputerChoice();
 
         if (playerSelection === "Rock") {
             switch (computerSelection) {
                 case "rock": 
+                    changeToRock();
                     resultPane.textContent = "Rock and Rock is a tie";
                     updateScore ();
                     break;
 
                 case "paper":
+                    changeToPaper();
                     computer += 1;
                     resultPane.textContent = "You lose! Paper beats Rock";
                     updateScore ();
                     break;
                 
                 case "scissor": 
+                    changeToScissor();
                     player += 1;
                     resultPane.textContent = "You win! Rock beats scissor";
                     updateScore ();
@@ -49,17 +53,20 @@
         else if (playerSelection === "Paper") {
             switch (computerSelection) {
                 case "rock": 
+                    changeToRock();
                     player += 1;
                     resultPane.textContent = "You win! Paper beats Rock";
                     updateScore ();
                     break;
 
                 case "paper":
+                    changeToPaper();
                     resultPane.textContent = "Paper and Paper is a tie";
                     updateScore ();
                     break;
                 
                 case "scissor": 
+                    changeToScissor();
                     computer += 1;
                     resultPane.textContent = "You lose! Scissor beats Paper";
                     updateScore ();
@@ -69,44 +76,57 @@
         else if (playerSelection === "Scissor") {
             switch (computerSelection) {
                 case "rock": 
+                    changeToRock();
                     computer += 1;
                     resultPane.textContent = "You lose! Rock beats Scissor";
                     updateScore ();
                     break;
 
                 case "paper":
+                    changeToPaper();
                     player += 1;
                     resultPane.textContent = "You win! Scissor beats Paper";
                     updateScore ();
                     break;
                 
                 case "scissor": 
+                    changeToScissor();
                     resultPane.textContent = "Scissor and Scissor is a tie";
                     updateScore ();
                     break;
             }
         }
 
-    function updateScore () {
-        resultScore.textContent = `You: ${player} Computer: ${computer}`;
-        if (player === 5) {
-            resultPane.textContent = "You Won!";
-            player = 0 ;
-            computer = 0 ;
-        }
-        if (computer === 5) {
-            resultPane.textContent = "Fucking loser";
-            player = 0 ;
-            computer = 0 ;
-        }
-    }
+}
 
+function updateScore () {
+    resultScore.textContent = `You: ${player} Computer: ${computer}`;
+    if (player === 5) {
+        resultPane.textContent = "You Won!";
+        player = 0 ;
+        computer = 0 ;
+    }
+    if (computer === 5) {
+        resultPane.textContent = "Fucking loser";
+        player = 0 ;
+        computer = 0 ;
+    }
+}
+function changeToRock(){
+    computerHand.setAttribute("src", "Images/Rock reversed.png")
+}
+function changeToPaper(){
+    computerHand.setAttribute("src", "Images/Paper reversed.png")
+}
+function changeToScissor(){
+    computerHand.setAttribute("src", "Images/Scissor reversed.png")
 }
 
     let choices = document.querySelectorAll('#btn') ;
     choices.forEach(choice => choice.addEventListener('click', singleRound));
     let resultPane = document.querySelector('#result');
     let resultScore = document.querySelector('#resultScore');
+    let computerHand = document.querySelector('.com-sign');
 
     
 
